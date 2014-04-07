6e153d7d2b5dd40a8667e91f65d5ec1cb537e3c5
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-04-07 13:40:46 -0300

    SharpMapTracker Released

---
+++ SharpTibiaProxy/Domain/ClientVersion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpTibiaProxy.Domain
+{
+    public class ClientVersion
+    {
+        public static readonly ClientVersion Version961 = new ClientVersion { Number = 961, FileVersion = "9.6.1.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 41 };
+        public static readonly ClientVersion Version963 = new ClientVersion { Number = 963, FileVersion = "9.6.3.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 42 };
+        public static readonly ClientVersion Version970 = new ClientVersion { Number = 970, FileVersion = "9.7.0.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 43 };
+        public static readonly ClientVersion Version981 = new ClientVersion { Number = 981, FileVersion = "9.8.1.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 45 };
+        public static readonly ClientVersion Version986 = new ClientVersion { Number = 986, FileVersion = "9.8.6.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 49 };
+        public static readonly ClientVersion Version1010 = new ClientVersion { Number = 1010, FileVersion = "10.1.0.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1011 = new ClientVersion { Number = 1011, FileVersion = "10.1.1.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1012 = new ClientVersion { Number = 1012, FileVersion = "10.1.2.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1013 = new ClientVersion { Number = 1013, FileVersion = "10.1.3.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1020 = new ClientVersion { Number = 1020, FileVersion = "10.2.0.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1030 = new ClientVersion { Number = 1030, FileVersion = "10.3.0.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1031 = new ClientVersion { Number = 1031, FileVersion = "10.3.1.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1032 = new ClientVersion { Number = 1032, FileVersion = "10.3.2.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1033 = new ClientVersion { Number = 1033, FileVersion = "10.3.3.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1034 = new ClientVersion { Number = 1034, FileVersion = "10.3.4.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Version1035 = new ClientVersion { Number = 1035, FileVersion = "10.3.5.0", OtbmVersion = 2, OtbMajorVersion = 3, OtbMinorVersion = 50 };
+        public static readonly ClientVersion Current = Version1035;
+
+        public int Number { get; private set; }
+        public string FileVersion { get; private set; }
+        public uint OtbmVersion { get; private set; }
+        public uint OtbMajorVersion { get; private set; }
+        public uint OtbMinorVersion { get; private set; }
+
+        private ClientVersion() { }
+
+        public static ClientVersion GetFromFileVersion(string fileVersion)
+        {
+            switch (fileVersion)
+            {
+                case "9.6.1.0": return Version961;
+                case "9.6.3.0": return Version963;
+                case "9.7.0.0": return Version970;
+                case "9.8.1.0": return Version981;
+                case "9.8.6.0": return Version986;
+                case "10.1.0.0": return Version1010;
+                case "10.1.1.0": return Version1011;
+                case "10.1.2.0": return Version1012;
+                case "10.1.3.0": return Version1013;
+                case "10.2.0.0": return Version1020;
+                case "10.3.0.0": return Version1030;
+                case "10.3.1.0": return Version1031;
+                case "10.3.2.0": return Version1032;
+                case "10.3.3.0": return Version1033;
+                case "10.3.4.0": return Version1034;
+                case "10.3.5.0": return Version1035;
+                default: return null;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as ClientVersion;
+            return other != null && other.Number == Number;
+        }
+
+        public override int GetHashCode()
+        {
+            return Number ^ 31;
+        }
+    }
+}
---
+++ SharpTibiaProxy/Domain/MessageClasses.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpTibiaProxy.Domain
+{
+    public enum MessageClasses
+    {
+        NONE = 0x00,
+
+        SPEAK_SAY = 0x01,
+        SPEAK_WHISPER = 0x02,
+        SPEAK_YELL = 0x03,
+        PRIVATE_FROM = 0x04,
+        PRIVATE_TO = 0x05,
+        CHANNEL_MANAGEMENT = 0x06,
+        CHANNEL = 0x07,
+        CHANNEL_HIGHLIGHT = 0x08,
+        SPEAK_SPELL = 0x09,
+        NPC_FROM_START_BLOCK = 0x0A,//10.37
+        NPC_FROM = 0x0A,
+        NPC_TO = 0x0B,
+        GAMEMASTER_BROADCAST = 0x0C,
+        GAMEMASTER_CHANNEL = 0x0E,//10.37
+        GAMEMASTER_PRIVATE_FROM = 0x0E,
+        GAMEMASTER_PRIVATE_TO = 0x0F,
+        SPEAK_MONSTER_SAY = 0x22,
+        SPEAK_MONSTER_YELL = 0x23,
+
+        SPEAK_FIRST = SPEAK_SAY,
+        SPEAK_LAST = GAMEMASTER_PRIVATE_TO,
+        SPEAK_MONSTER_FIRST = SPEAK_MONSTER_SAY,
+        SPEAK_MONSTER_LAST = SPEAK_MONSTER_YELL,
+
+        STATUS_CONSOLE_BLUE = 0x04, /*Teal message in local chat*/
+        STATUS_CONSOLE_RED = 0x0C, /*Red message in console*/
+        STATUS_DEFAULT = 0x10, /*White message at the bottom of the game window and in the console*/
+        STATUS_WARNING = 0x11, /*Red message in game window and in the console*/
+        EVENT_ADVANCE = 0x12, /*White message in game window and in the console*/
+        STATUS_SMALL = 0x13, /*White message at the bottom of the game window"*/
+        INFO_DESCR = 0x14, /*Green message in game window and in the console*/
+        DAMAGE_DEALT = 0x15,
+        DAMAGE_RECEIVED = 0x16,
+        HEALED = 0x17,
+        EXPERIENCE = 0x18,
+        DAMAGE_OTHERS = 0x19,
+        HEALED_OTHERS = 0x1A,
+        EXPERIENCE_OTHERS = 0x1B,
+        EVENT_DEFAULT = 0x1C, /*White message at the bottom of the game window and in the console*/
+        LOOT = 0x1D, /*Green message in game window and in the console*/
+        TRADE_NPC = 0x1E, /*Green message in game window and in the console*/
+        EVENT_GUILD = 0x1F, /*Green message in game window and in the console*/
+        PARTY_MANAGEMENT = 0x20, /*Green message in game window and in the console*/
+        PARTY = 0x21, /*Green message in game window and in the console*/
+        EVENT_ORANGE = 0x22, /*Orange message in local chat*/
+        STATUS_CONSOLE_ORANGE = 0x23, /*Orange message in local chat*/
+        REPORT = 0x24, /*White message in game window and in the console*/
+        HOTKEY_USE = 0x25, /*Green message in game window*/
+        TUTORIAL_HINT = 0x26
+    };
+}
